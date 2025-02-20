32456f2c2a0839745af22dad9a37ed895ab33ed5
pq <Velvetrose13@mail.ru>
2025-02-20 21:52:53 +0300

    New Kazz

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
+import './Header.css';
 
 const Header = () => {
     const {user, onClose} = useTelegram();
 
-    return 
-    (
+    return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}> 
+            <span className={'username'}>
                 {user?.username}
-                </span>
+            </span>
         </div>
     );
 };
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -14,12 +14,11 @@ export function useTelegram() {
         }
     }
 
-
-
     return {
         onClose,
         onToggleButton,
         tg,
         user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
     }
 }
