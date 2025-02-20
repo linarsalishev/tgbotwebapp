9feb59fc5a608191ad79f1e99b549155813a9da7
pq <Velvetrose13@mail.ru>
2025-02-20 21:32:18 +0300

    yeap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useEffect } from 'react';
 import './App.css';
+import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
 
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
+
   useEffect(() => {
     tg.ready();
-  }, [])
+  }, []) 
 
 
 const onClose = () => {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
     const {user, onClose} = useTelegram();
 
-    return (
+    return 
+    (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}> {user?.username} </span>
-
+            <span className={'username'}> 
+                {user?.username} 
+                </span>
         </div>
     );
 };
