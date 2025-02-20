fe2b47f13ccd632cc52f4e03bc34fc84a890c83a
pq <Velvetrose13@mail.ru>
2025-02-20 20:28:22 +0300

    initial commit

---
+++ src/components/buttuons/Header/header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const header = () => {
+
+    const tg = window.Telegram.WebApp;
+
+
+    const onClose = () => {
+        tg.close()
+    }
+
+    return (
+        <div>
+
+            <button onClick={onClose}>Закрыть</button>
+            <span className={'username'}> {tg.initDataUnsafe?.user?.username} </span>
+
+        </div>
+    );
+};
+
+export default header;
