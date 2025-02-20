9d45fb960a6c6d6f37718c60759c06c9ec13eaeb
pq <Velvetrose13@mail.ru>
2025-02-20 21:36:23 +0300

    asdf

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     return 
     (
         <div className={'header'}>
-            <Button onClick={onClose}>Закрыть</Button>
+            <button onClick={onClose}>Закрыть</button>
             <span className={'username'}> 
                 {user?.username} 
                 </span>
